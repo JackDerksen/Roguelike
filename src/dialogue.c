de6425ac3407e7f0155c7fcd1373dfaf950e1351
JackDerksen <jmderksen9@gmail.com>
2023-11-29 20:09:44 -0700

    Deleted unnecessary dialogue files

--- src/dialogue.c
+++
@@ -1,6 +0,0 @@
-#include "dialogue.h"
-
-// This file will probably just contain a large-ish dictionary of dialogue
-// options relevant to whichever type of player will speak it (maybe even
-// some generic game subtitles), and some code to select and display the
-// dialogue.
